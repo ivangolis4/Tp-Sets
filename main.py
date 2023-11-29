11ffd4f6bb1121df0a33c4f62d4c9dca84453942
BriTZ <ivangolis4@gmail.com>
2023-11-29 09:03:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,58 @@
-students = {}
+import re
+
+students = []
+pattern = re.compile(r'^\d{4}-\d{4}$')
+name_pattern = re.compile(r'[a-zA-Z]')
 
 def length():
     try:
-        return int(input("How many students do you like to enroll:"))
+        return int(input("How many students do you like to enroll: "))
     except ValueError:
         return length()
 
+def print_list():
+    print("=====================================")
+    print("Student List:")
+    for student in students:
+        print(student['id'], student['name'])
 
+def update_students():
+    new_id = input("Enter new Student number: ")
+    if pattern.match(new_id):
+        while True:
+            new_name = input("Enter new first name: ")
+            if name_pattern.match(new_name):
+                students[2] = {'id': new_id, 'name': new_name}
+                print_list()
+                break
+            else:
+                print("Please enter a proper name.")
+    else:
+        print("Invalid ID format.")
 
+isn = length()
+if isn < 3:
+    print("=====================================")
+    print("Must have at least 3 students.")
+    isn = length()
+print("=====================================")
 
-i = length()
+for i in range(isn):
+    bl = True
+    while bl:
+        student_id = input("Enter Student number " + str(i + 1) + ": ")
+        if pattern.match(student_id):
+            while True:
+                student_name = input("Enter first name " + str(i + 1) + ": ")
+                if name_pattern.match(student_name):
+                    students.append({'id': student_id, 'name': student_name})
+                    bl = False
+                    break
+                else:
+                    print("Invalid Name.")
+        else:
+            print("Invalid ID.")
 
-for i in range(i):
-    i = i + 1
-    id = input("Enter Student number "+str(i)+": ")
-    name = input("Enter first name "+str(i)+": ")
-    students[id] = name
-
-
-print("Student List;")
-for keys, value in students.items():
-    print(keys, value)
-
-
-id = input("Enter Student number: ")
-name = input("Enter first name: ")
-students[id] = name
-
-print("Student List;")
-for keys, value in students.items():
-    print(keys, value)
+print_list()
+print("=====================================")
+update_students()
